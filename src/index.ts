28e6fd5f03eec3a3668f17b3bcd27d0cf08a3e74
Ben Crevis <benjamin.crevis@bp.com>
2021-05-07 10:44:57 +0100

    Add documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-import jsonFile from 'jsonfile';
+import jsonFile from "jsonfile";
 import builder from "junit-report-builder";
 import { Feature, Scenario, StepStatus } from "./model/cucumber";
 import { Config } from "./model/config";
 
-export class CucumberConverter {
-  constructor(private config: Config) {}
+const nsToSecondsConversion = 1000000000;
 
+/**
+ * Converts Cucumber result json files to JUnit XML.
+ *
+ * @example
+ * const converter = new CucumberConverter({
+ *    markUndefinedAsFailed: true
+ * });
+ * converter.convertToJunit('path/to/cucumber.json', 'path/to/output.xml');
+ *
+ */
+export class CucumberConverter {
+  /**
+   * @param config - Configuration options for the converter. Default configuration is:
+   * `{ markUndefinedAsFailed: true }`.
+   */
+  constructor(private config: Config = { markUndefinedAsFailed: true }) {}
+
+  /**
+   * Converts Cucumber result json files to JUnit XML.
+   *
+   * @param inputFilePath - The input Cucumber json results file. E.g 'path/to/cucumber.json'
+   * @param outputFilePath - The output JUnit XML file. E.g 'path/to/output.xml'.
+   * This will overwrite any file of the same name
+   *
+   */
   public convertToJunit(inputFilePath: string, outputFilePath: string): void {
     const cucumberReport: Feature[] = jsonFile.readFileSync(inputFilePath);
 
@@ -21,27 +45,31 @@ export class CucumberConverter {
     const suite = builder.testSuite().name(feature.name);
 
     for (const scenario of feature.elements) {
-      suiteDuration += this.addTestCases(scenario, suite);
+      suiteDuration += this.addTestCase(scenario, suite);
     }
 
     suite.time(suiteDuration);
   }
 
-  private addTestCases(scenario: Scenario, suite: any): number {
-    if (scenario.type === 'background') {
+  private addTestCase(scenario: Scenario, suite: any): number {
+    if (scenario.type === "background") {
       return 0;
     }
 
     const scenarioResult = this.getScenarioResult(scenario);
 
-    const testCase = suite.testCase()
+    const testCase = suite
+      .testCase()
       .name(scenario.name)
       .className(scenario.id)
       .time(scenarioResult.duration);
 
-    if (scenarioResult.status === 'failed') {
-        testCase.failure(scenarioResult.failureMessage);
-    } else if (scenarioResult.status === 'skipped' || scenarioResult.status === 'undefined') {
+    if (scenarioResult.status === "failed") {
+      testCase.failure(scenarioResult.failureMessage);
+    } else if (
+      scenarioResult.status === "skipped" ||
+      scenarioResult.status === "undefined"
+    ) {
       testCase.skipped();
     }
 
@@ -49,24 +77,28 @@ export class CucumberConverter {
   }
 
   private getScenarioResult(scenario: Scenario) {
-    let scenarioStatus: StepStatus = 'passed';
-    let failureMessage = '';
+    let scenarioStatus: StepStatus = "passed";
+    let failureMessage = "";
     let scenarioDuration = 0;
 
     for (const step of scenario.steps) {
-      scenarioDuration += (step.result.duration ? step.result.duration / 1000000000 : 0); //ns to seconds;
+      scenarioDuration += step.result.duration
+        ? step.result.duration / nsToSecondsConversion
+        : 0;
 
-      if(scenarioStatus === 'failed') {
+      if (scenarioStatus === "failed") {
         continue;
       }
 
-      if (step.result.status === 'failed') {
-        scenarioStatus = 'failed';
+      if (step.result.status === "failed") {
+        scenarioStatus = "failed";
         failureMessage = step.result.error_message!;
-      }
-      else if (this.config.markUndefinedAsFailed && step.result.status === 'undefined') {
-        scenarioStatus = 'failed';
-        failureMessage = 'One or more steps are undefined';
+      } else if (
+        this.config.markUndefinedAsFailed &&
+        step.result.status === "undefined"
+      ) {
+        scenarioStatus = "failed";
+        failureMessage = "One or more steps are undefined";
       } else {
         scenarioStatus = step.result.status;
       }
